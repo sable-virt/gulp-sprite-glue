177b300bef55e8a447351b7172b8084ffaacd159
frontainer <frontainer@gmail.com>
2014-10-03 21:48:24 +0900

    gulp実行サンプルを追加

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var glue = require('./index');
+var gulp = require('gulp');
+
+/**
+ * デフォルトタスク
+ */
+gulp.task('default', function () {
+    gulp.src('./test/images')
+        .pipe(glue('./test'));
+});
+
+gulp.task('separate', function () {
+    gulp.src('./test/images')
+        .pipe(glue({
+            css: './test/css',
+            img: './test/sprite'
+        }));
+});
